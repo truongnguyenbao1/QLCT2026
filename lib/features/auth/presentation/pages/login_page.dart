4bb4b46c157abe5758ae663eeac88d01bdfdc1f8
Bao <nguyenbaotruong160@gmail.com>
2026-07-31 12:09:59 +0700

    v120931072026

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -67,9 +67,7 @@ class _LoginPageState extends State<LoginPage> {
       await sb.Supabase.instance.client.auth.signInWithOAuth(
         sb.OAuthProvider.google,
         redirectTo: redirectTo,
-        authScreenLaunchMode: (!kIsWeb && (Platform.isWindows || Platform.isMacOS || Platform.isLinux))
-            ? LaunchMode.externalApplication
-            : LaunchMode.platformDefault,
+        authScreenLaunchMode: kIsWeb ? LaunchMode.platformDefault : LaunchMode.externalApplication,
       );
     } catch (e) {
       if (mounted) {
